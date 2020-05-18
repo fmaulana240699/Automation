cc2fff1ce0923961de2382a0de7500de3685c8f8
Your Name <you@example.com>
2020-05-18 16:50:04 +0700

    deploy.yaml

--- build.py
+++ build.py
@@ -9,7 +9,14 @@ serv=os.popen("kubectl get svc | grep -i python ")
 services= serv.read()
 
 if deployment == None:
-    os.system("ansible-playbook build.yaml && rm -rf Automation")
-else:
-    os.system("ansible-playbook update.yaml && rm -rf Automation")
+    os.system("ansible-playbook build.yaml")
+    verif=os.popen("kubectl get deployment")
+    verify=verif.read()
 
+    if verify == None:
+        print("Build Aplikasi GAGAL")
+    else:
+       print("Build Aplikasi BERHASIL")
+else:
+    os.system("ansible-playbook update.yaml")
+    print("Update Aplikasi BERHASIL")
