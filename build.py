3ca30917cc643bd7e471c362e44b50331d7cb157
Your Name <you@example.com>
2020-05-19 20:20:17 +0700

    fix

--- build.py
+++ build.py
@@ -1,49 +1,25 @@
 import os
-import subprocess
-import sys
 
-sshProcess = subprocess.Popen(['ssh',"ec2-user@master"],stdin=subprocess.PIPE,stdout = subprocess.PIPE,universal_newlines=True,bufsize=0)
-sshProcess.stdin.write("git clone https://github.com/fmaulana240699/Automation.git \n")
-sshProcess.stdin.close()
+deploy=os.popen("kubectl get deployment | grep -i python")
+deployment= deploy.read()
 
-sshProcess = subprocess.Popen(['ssh',"ec2-user@master"],stdin=subprocess.PIPE,stdout = subprocess.PIPE,universal_newlines=True,bufsize=0)
-sshProcess.stdin.write("kubectl get deployment | grep -i python \n")
-sshProcess.stdin.close()
-
-output=sshProcess.stdout
-deployment=output.read()
-print(deployment)
-
-sshProcess = subprocess.Popen(['ssh',"ec2-user@master"],stdin=subprocess.PIPE,stdout = subprocess.PIPE,universal_newlines=True,bufsize=0)
-sshProcess.stdin.write("kubectl get svc | grep -i python")
-sshProcess.stdin.close()
-
-output2=sshProcess.stdout
-service=output2.read()
-print(service)
-
-
+serv=os.popen("kubectl get svc | grep -i python ")
+services= serv.read()
 
 if deployment == None:
-    sshProcess = subprocess.Popen(['ssh',"ec2-user@master"],stdin=subprocess.PIPE,stdout = subprocess.PIPE,universal_newlines=True,bufsize=0) 
-    sshProcess.stdin.write("ansible-playbook build.yaml \n")
-    sshProcess.stdin.close()
+    os.system("kubectl create -f deployment.yaml && kubectl create -f services.yaml")
+    os.system("cd .. && rm -rf Automation")
 
-    output3=sshProcess.stdout
-    hasil=output3.read()
-    print(hasil)
+    s=os.popen("kubectl get deployment | grep -i python")
+    success= s.read()
+
+    if success == None:
+        print("Build App Gagal")
+        terminate()
+    else:
+        print("Build App Berhasil")
 
 else:
-    sshProcess = subprocess.Popen(['ssh',"ec2-user@master"],stdin=subprocess.PIPE,stdout = subprocess.PIPE,universal_newlines=True,bufsize=0) 
-    sshProcess.stdin.write("ansible-playbook update.yaml \n")
-    sshProcess.stdin.close()
-
-    output4=sshProcess.stdout
-    hasil2=output4.read()
-    print(hasil2)
-   
-
-
-
-
-
+    os.system("kubectl apply -f deployment.yaml")
+    os.system("cd .. && rm -rf Automation")
+    print("Update Aplikasi Berhasil")
